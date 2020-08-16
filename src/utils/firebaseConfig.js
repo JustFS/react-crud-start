f45476d9fd39bd2e0f5d83f8c5003cc937191512
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-08-16 18:27:56 +0200

    added sass

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,12 +3,11 @@ import firebase from 'firebase';
 const firebaseConfig = {
   apiKey: "AIzaSyA4wDktmix3h-uplpwog89tf1ynzxpAL_k",
   authDomain: "react-video-auth.firebaseapp.com",
-
-  // databaseURL: "https://react-video-auth.firebaseio.com",
-  // projectId: "react-video-auth",
-  // storageBucket: "react-video-auth.appspot.com",
-  // messagingSenderId: "708497657186",
-  // appId: "1:708497657186:web:a70c884bf76f86021e703b"
+  databaseURL: "https://react-video-auth.firebaseio.com",
+  projectId: "react-video-auth",
+  storageBucket: "react-video-auth.appspot.com",
+  messagingSenderId: "708497657186",
+  appId: "1:708497657186:web:a70c884bf76f86021e703b"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
